38fcebe151722b6d54bda471c82bd25e13e1126f
tungvan1905 <47826721+tungvan1905@users.noreply.github.com>
2022-01-26 14:47:31 +0700

    Add files via upload

---
+++ src/App.vue
@@ -0,0 +1,28 @@
+<template>
+	<div id="app">
+		<Navbar />
+		<Todos />
+	</div>
+</template>
+
+<script>
+import Todos from './components/Todos'
+import Navbar from './components/Navbar'
+
+export default {
+	name: 'App',
+	components: { Todos, Navbar }
+}
+</script>
+
+<style>
+#app {
+	font-family: Avenir, Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	background: rgb(219, 219, 219);
+	margin: 20px auto;
+	width: 90%;
+	max-width: 800px;
+}
+</style>
---
+++ src/components/Navbar.vue
@@ -0,0 +1,39 @@
+<template>
+	<div class="navbar">
+		<h1>My Vuex App Todos</h1>
+		<ul>
+			<li>Home</li>
+			<li>About</li>
+			<li >
+				Total todos: {{ todos.length }}
+			</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+import { mapMutations, mapGetters } from 'vuex'
+
+export default {
+	name: 'Navbar',
+	computed: mapGetters(['todos']),
+	methods: mapMutations(['TOGGLE_AUTH'])
+}
+</script>
+
+<style>
+.navbar {
+	padding: 5px;
+	text-align: center;
+	background: rgb(240, 240, 240);
+	color: black;
+}
+
+.navbar ul {
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+	list-style: none;
+	padding: 0;
+}
+</style>
